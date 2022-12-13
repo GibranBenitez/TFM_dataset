925302670b471d1b129f27ca99ba9347694d30ef
Agus Castillo <agu13xd@hotmail.com>
2022-12-13 15:55:46 -0600

    Modificando final

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
-import argparse 
+import argparse
 from yolov5.detect import run
 import os
 from os import path as osPath
 from SSD.detect import runssd
-from FCOS_torch.detect import detect
-checkpoints = {'yolov5':'checkpoints\yolo.pt','retina':'checkpoints\weight_retina.pth.rar',
-            'fcos':'checkpoints\weight_fcos.pth.rar','ssd':'checkpoints\ssd.pth'}
- 
+from FCOS_RETINA import detect
+checkpoints = {'yolov5': 'checkpoints\yolo.pt', 'retina': 'checkpoints\weight_retina.pth.rar',
+               'fcos': 'checkpoints\weight_fcos.pth.rar', 'ssd': 'checkpoints\ssd.pth'}
+
 
 def checkdir(filePath):
-
 
     if os.path.exists(filePath):
         numb = 1
         while True:
-            
-            newName = "{0}".format(filePath+'(' + str(numb) +')')
 
-            
+            newName = "{0}".format(filePath+'(' + str(numb) + ')')
+
             if os.path.exists(newName):
                 numb += 1
             else:
-                
+
                 return newName
-    
+
     return filePath
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='yolov5',required=True)
-    parser.add_argument('--source', type=str, default='./data',required=True)
+    parser.add_argument('--model', type=str, default='yolov5', required=True)
+    parser.add_argument('--source', type=str, default='./data', required=True)
     args = parser.parse_args()
 
     if not os.path.exists('./runs'):
         os.makedirs('./runs', exist_ok=True)
 
     source = args.source
-    
 
     if not os.path.exists(source):
         print("Error Dir not found")
@@ -48,34 +45,33 @@ if __name__ == '__main__':
         if args.model == 'yolov5':
             path = "./runs/yolov5"
             path = checkdir(path)
-            #print (path)  
-            run(weights = checkpoints['yolov5'],source=args.source,project=path,name='./')
+            #print (path)
+            run(weights=checkpoints['yolov5'],
+                source=args.source, project=path, name='./')
 
         if args.model == 'ssd':
             path = "./runs/ssd"
             path = checkdir(path)
             if not os.path.exists(path):
                 os.makedirs(path, exist_ok=True)
-            #print(path)
-            runssd(source,path)
-            print("\nResults saved to: ",path)
+            # print(path)
+            runssd(source, path)
+            print("\nResults saved to: ", path)
 
         if args.model == 'retina':
             path = "./runs/retina"
             path = checkdir(path)
             if not os.path.exists(path):
                 os.makedirs(path, exist_ok=True)
-        
-            detect(checkpoints['retina'],source, 0.5, path,'retina')
-            print("\nResults saved to: ",path)
+
+            detect(checkpoints['retina'], source, 0.5, path, 'retina')
+            print("\nResults saved to: ", path)
 
         if args.model == 'fcos':
             path = "./runs/fcos"
             path = checkdir(path)
             if not os.path.exists(path):
                 os.makedirs(path, exist_ok=True)
-        
-            detect(checkpoints['fcos'],source, 0.5, path)
-            print("\nResults saved to: ",path)
-        
 
+            detect(checkpoints['fcos'], source, 0.5, path)
+            print("\nResults saved to: ", path)
